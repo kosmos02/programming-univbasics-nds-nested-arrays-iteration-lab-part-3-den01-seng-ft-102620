fcb2349fc3eef7413e33e84a29ef6ec20cd16247
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-05 17:20:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,7 +13,7 @@ def join_nested_strings(src)
       
       if src[row_index][element_index].is_a?(String)
         
-        result += src[row_index][element_index]
+        result += src[row_index][element_index] + " "
         
       end
       
